5a126a07004993abab749c3d79cd041a737ed2a0
Jonathan <aguilarjonathan2902@gmail.com>
2024-10-17 21:59:25 -0300

    Creamos las entidades e implementamos ORM para la generacion de tablas en la base de datos: Curso - Estudiante - Profesor

---
+++ src/main/java/com/educacionit/elearningcourses/entity/Profesor.java
@@ -0,0 +1,25 @@
+package com.educacionit.elearningcourses.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="profesores")
+public class Profesor {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="prof_id")
+    private Long id;
+    @Column(name="prof_nombre")
+    private String nombre;
+    @Column(name="prof_especialidad")
+    private String especialidad;
+    @OneToMany(mappedBy = "profesor", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Curso> cursos;
+}
